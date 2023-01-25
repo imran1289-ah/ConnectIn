b76711d8b8e1673d390d877ba3644609d8635575
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-01-25 15:54:30 -0500

    adding a simple unit test for the sign in page

--- src/client/src/components/SignIn.jsx
+++ src/client/src/components/SignIn.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/signin.css";
 
 const SignIn = () => {
+  //State for each input
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  //HTTP Request to autenticate user
+  const submitLogin = async (e) => {};
+
   return (
     //Signin page
     <div className="Wrapper">
       <div className="Form">
         <h3 className="Title">Log in to your account </h3>
-        <form>
+        <form onSubmit={submitLogin}>
           <div className="Form">
-            <span className="Placeholder">Email or Username</span>
+            <label className="Placeholder">
+              Email or Username
+              <br></br>
+              <input
+                className="Input"
+                placeholder="Enter your email or username"
+                onChange={(e) => setUser(e.target.value)}
+              ></input>
+            </label>
             <br></br>
-            <input
-              className="Input"
-              placeholder="Enter your email or username"
-            ></input>
             <br></br>
-            <br></br>
-            <span className="Placeholder">Password</span>
-            <br></br>
-            <input className="Input" placeholder="Enter your password"></input>
+            <label className="Placeholder">
+              Password
+              <br></br>
+              <input
+                className="Input"
+                placeholder="Enter your password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </label>
             <br></br>
             <br></br>
             <div className="Link">Dont Have an account ? Sign Up</div>

5eee0b38bacbd23184ec37319f784a5fdadc3b91
Joe El-Khoury <joe.elkhoury.07@gmail.com>
2023-01-25 23:10:39 -0500

    Removed username

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -17,10 +17,6 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    username: {
-        type: String,
-        required: true
-    },
     password:{
         type: String,
         required: true

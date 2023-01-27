0424ce26cdad2dacb852d66cfed01934ea637a3a
Joe El-Khoury <joe.elkhoury.07@gmail.com>
2023-01-27 18:01:08 -0500

    Implemented the created user functionality

--- src/server/controllers/usersController.js
+++ src/server/controllers/usersController.js
@@ -1,8 +1,33 @@
-
 const User = require('../models/user');
-const createUser = async (req, res) =>{
-            
-}
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+
+
+const createUser = asyncHandler(async (req, res) =>{
+    const {firstname, lastname, email, password} = req.body
+    
+    // Checking if all fields are filled.
+    if (!firstname || !lastname || !email || !password) {
+        return res.status(400).json({ message: 'Please fill out all fields!'})
+    }
+    
+    // Checks if a duplicate user exists on the database?
+    const isThereADuplicate = await User.findOne({email}).lean().exec();
+    if (isThereADuplicate) {
+        return res.status(409).json({message: 'A user with this email already exists.'})
+    }
+    
+    const hashPwd = await bcrypt.hash(password,10)
+    const userDocument = {firstname,lastname,email,"password": hashPwd}
+    const newUser = await User.create(userDocument)
+    
+    if (newUser) {
+        res.status(201).json({message: 'User successfully created!'})
+    } else {
+        res.status(400).json({message: 'User unsuccessfully created.'})
+    }
+
+})
 
 const getUser = async (req, res) => {
 

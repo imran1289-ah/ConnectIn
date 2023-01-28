f73ea8d80731980487c85d2ae1d7812f50785acd
AnnaHsu1 <annahsu2000@hotmail.com>
2023-01-27 19:02:33 -0500

    Added comments to usersController

--- src/server/controllers/usersController.js
+++ src/server/controllers/usersController.js
@@ -5,14 +5,21 @@ const createUser = async (req, res) =>{
             
 }
 
+// This action returns the user based on the email
 const getUserByEmail = async (req, res) => {
-    const user = await User.find().select('-password').lean()
-    if(!user?.length){
-        return res.status(400).json({message:"No user found"})
-    } 
-    res.json(user)
+    const user = await User.findOne({email: req.body.email}).then(user => {
+        if (user) {
+            console.log(`Found user ${user.email}`)
+            res.status(200).json(user)
+        }
+        
+        else {
+            return res.status(400).json({message:"No user found"})
+        }
+    })
 }
 
+// This action is to verify the credentials of the user when logging in
 const verifyUser = async (req, res) => {
 
     const user = await User.findOne({email: req.body.email, password: req.body.password}).then(user => {   

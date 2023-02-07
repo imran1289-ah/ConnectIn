b8eba120f9028f3d449bd915fe18f6d24ff36572
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-02-07 14:45:21 -0500

    starting to build the profile page

--- src/client/src/components/PublicUserProfile.jsx
+++ src/client/src/components/PublicUserProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import "../css/publicuserprofile.css";
 
 const PublicUserProfile = () => {
   //States
@@ -26,8 +27,69 @@ const PublicUserProfile = () => {
   }, [locationURL]);
 
   return (
-    <div>
-      {publicUser.firstname} {publicUser.lastname}
+    <div className="userProfileContainer">
+      <div className="left">
+        <div className="userInformation">
+          <div className="userContainer">
+            <img
+              src="https://i.pinimg.com/originals/f1/0f/f7/f10ff70a7155e5ab666bcdd1b45b726d.jpg"
+              alt="default pic"
+              className="userProfilePic"
+            ></img>
+            <div className="userAboutSection">
+              <span className="userName">
+                {publicUser.firstname} {publicUser.lastname}
+              </span>
+              <br />
+              <p>
+                Software Engineering Student At Concordia University In Montreal
+                Quebec
+              </p>
+              <button>Connect</button>
+            </div>
+          </div>
+        </div>
+        <div className="userInformation">
+          Experience
+          <br></br>
+          <ul>
+            <l1 className="jobInfo">
+              <img
+                src="https://play-lh.googleusercontent.com/5pZMqQYClc5McEjaISPkvhF8pDmlbLqraTMwk1eeqTlnUSjVxPCq-MItIrJPJGe7xW4"
+                alt="comapnyPic"
+                className="companyPic"
+              ></img>
+              <div>
+                <span>Software Engineer</span>
+                <br></br>
+                <span>Facebook</span>
+                <br></br>
+                <span>2007-2019</span>
+              </div>
+            </l1>
+          </ul>
+        </div>
+        <div className="userInformation">
+          Education
+          <ul>
+            <l1 className="jobInfo">
+              <img
+                src="https://concordiabootcamps.ca/wp-content/uploads/2019/03/icon.png"
+                alt="comapnyPic"
+                className="companyPic"
+              ></img>
+              <div>
+                <span>Software Engineering COOP</span>
+                <br></br>
+                <span>Concordia University</span>
+                <br></br>
+                <span>2020-2024</span>
+              </div>
+            </l1>
+          </ul>
+        </div>
+      </div>
+      <div className="right">Contacts</div>
     </div>
   );
 };

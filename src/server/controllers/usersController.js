4b080b3437a4a142e32e369163ff2bbe51bd9b7e
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2023-02-09 12:04:40 -0500

    Update usersController.js

--- src/server/controllers/usersController.js
+++ src/server/controllers/usersController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const user = require("../models/user");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 var sesh;

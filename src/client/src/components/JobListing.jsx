835fc994c07fd836a360d2ac77c683fd2a8bc459
DasDeveloper <rohandas@live.ca>
2023-02-14 23:17:10 -0500

    Display all jobs in JobListing page

--- src/client/src/components/JobListing.jsx
+++ src/client/src/components/JobListing.jsx
@@ -1,51 +1,50 @@
 import React from "react";
 import axios from "axios"
 import "../css/jobListing.css";
+import {useEffect, useState} from "react";
 
 
-const getJobListing = () =>{
+const JobListing = () =>{
+    const [jobs, setJobs] = useState([]);
 
-    const jobs = axios.get("http://localhost:9000/jobs")
-    
+    useEffect( () => {
+        fetchData()
+    }, [])
 
-}
+    const fetchData = async () => {
+        const {data} = await axios.get("http://localhost:9000/jobs")
 
+        setJobs(data)
+    }
 
-function getJobs() {
-    const [jobsData, setJobsData] = useState({
-      job_id: null,
-      description: "",
-      salary: null,
-      company: "",
-      catagory: "",
-      title: ""
-    });
-}
-  
-const jobListing = () =>{
-    
     
     return(
             
         <div className="Container">
             
+            
             <div className="jobPosts">
                 <div className="heading">
                     <b>Job Posts</b>
                 </div>
+
+                {jobs.map(jobs => (
+                    <div className="jobPost">
+                        <p> Job id: {jobs.job_id}</p>
+                        <p>Title: {jobs.title}</p>
+                        <p>Description: {jobs.description}</p>
+                        <p>Salary: {jobs.salary}</p>
+                        <p>Company: {jobs.company}</p>
+                        <p>Category: {jobs.category}</p>
+                        <button> Select</button>
+                    </div>
+                ))}
                
-                <div className = "jobPost">
-                    
-                   <p>Job title </p>
-                   <p>Job description </p>
-                   <p>salary</p>
-                   <p>contract type </p>
-                   <p>Other information </p>
-                   <button> Select</button>
-                </div>
+
+                
 
 
-               </div>
+            </div>
 
             <div className="preferences">
                     <b>Preferences</b>
@@ -60,4 +59,4 @@ const jobListing = () =>{
 
 
 
-export default jobListing;
+export default JobListing;

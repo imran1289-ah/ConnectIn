083afd91c9de56b81a367aa45de71a150d845a60
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-02-16 07:56:04 -0500

    potential fix for the seach controller issue

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const session = require("express-session");
 const cors = require("cors");
+const User = require("./models/user");
 
 const app = express();
 app.use(express.json());
@@ -12,10 +13,10 @@ dotenv.config();
 
 //Cors middleware to accept request from client
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-        credentials: true,
-    })
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
 );
 
 //Supress DeprecationWarning message
@@ -23,26 +24,40 @@ mongoose.set("strictQuery", true);
 
 //Database connection (MongoDB Atlas)
 const dbConnect = () => {
-    mongoose
-        .connect(process.env.DATABASE)
-        .then(() => {
-            console.log("Database Connected Successfully");
-        })
-        .catch((err) => {
-            throw err;
-        });
+  mongoose
+    .connect(process.env.DATABASE)
+    .then(() => {
+      console.log("Database Connected Successfully");
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 app.use("/users", require("./routes/userRoutes.js"));
 app.use("/resume", require("./routes/uploadResumeCL.js"));
 
+app.get("/search", async (req, res) => {
+  const firstname = req.query.term;
+
+  const users = await User.find({
+    firstname: { $regex: firstname, $options: "i" },
+  }).then((users) => {
+    if (users) {
+      res.status(200).json(users);
+    } else {
+      return res.status(400).json({ message: "No user exists with this name" });
+    }
+  });
+});
+
 //Running the server
 const server = app.listen(port, () => {
-    dbConnect();
-    // console.log(`Server listening on port ${port}`);
+  dbConnect();
+  // console.log(`Server listening on port ${port}`);
 });
 
 module.exports = server;
--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -3,26 +3,24 @@ const router = express.Router();
 const usersControllers = require("../controllers/usersController");
 
 router
-    .route("/")
-    .get(usersControllers.verifyUser)
-    .post(usersControllers.createUser)
-    .patch(usersControllers.updateUser)
-    .delete(usersControllers.deleteUser);
+  .route("/")
+  .get(usersControllers.verifyUser)
+  .post(usersControllers.createUser)
+  .patch(usersControllers.updateUser)
+  .delete(usersControllers.deleteUser);
 
 router
-    .route("/:_id")
-    .get(usersControllers.getUser)
-    .patch(usersControllers.updateUser)
-    .delete(usersControllers.deleteUser);
+  .route("/:_id")
+  .get(usersControllers.getUser)
+  .patch(usersControllers.updateUser)
+  .delete(usersControllers.deleteUser);
 
 router.route("/signin").post(usersControllers.verifyUser);
 
-
 //route to search users
-router.route("/search").get(usersControllers.search);
+// router.route("/search").get(usersControllers.search);
 
 //router to fetch user info for profile page
 router.route("/profile/:id").get(usersControllers.getUserInfo);
 
 module.exports = router;
-

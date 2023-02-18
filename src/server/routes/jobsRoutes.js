3d1b61a43494173c7a125ef040d749fe1bf51824
DasDeveloper <rohandas@live.ca>
2023-02-17 21:47:19 -0500

    Removed unnecessary comments

--- src/server/routes/jobsRoutes.js
+++ src/server/routes/jobsRoutes.js
@@ -4,30 +4,9 @@ const jobsControllers = require("../controllers/jobsController");
 const Job = require("../models/Job");
 
 
-//returns all jobs available
-// router.get('/', async (req, res) => {
 
-//   try{
-//     const jobs = await Job.find();
-//     res.status(200).json(jobs);
-//   }catch(err){
-//       res.status(500).json( {message: "Unable to retrieve all jobs."});
-//       console.log(err);
-//   }
-// })
 router.get('/', jobsControllers.getAllJobs)
 
-//returns all information of a specific job_id
-// router.get('/:jobId', async (req, res) => {
-
-//     try{
-//         const job = await Job.findOne({job_id: req.params.jobId});
-//         res.send(job);
-//     }catch(err){
-//         res.status[400].json({message: "Something went wrong."})
-//     }
-  
-// })
 
 router.get('/:jobId', jobsControllers.getJobDetails)
 

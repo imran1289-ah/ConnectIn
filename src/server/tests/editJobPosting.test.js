f8c43446e217678c8f111dc882d779a647b2b6e2
Joe El-Khoury <joe.elkhoury.07@gmail.com>
2023-02-18 18:06:20 -0500

    Improved editJobPosting test

--- src/server/tests/editJobPosting.test.js
+++ src/server/tests/editJobPosting.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
-const app = require('../index')
-const mongoose = require("mongoose")
+const app = require('../index');
+const mongoose = require("mongoose");
+const Job = require('../models/Job');
 
 // Connects to the DB
 beforeAll(() => {
- mongoose.connect(process.env.DATABASE)
+    mongoose.connect(process.env.DATABASE)
 })
 
 // Closing the DB connection allows Jest to exit successfully.
@@ -15,8 +16,22 @@ afterAll((done) => {
 })
 
 describe('GET /jobs', function(){
+
+    let jobId;
+    const createJob = new Job({
+        job_id: 12345,
+        title: "Test Title",
+        description: "This is a test description.",
+        salary: 95000,
+        company: "ConnectIn",
+        category: "Part-Time",
+        location: "Hybrid"
+    });
+    createJob.save();
+    jobId = createJob.job_id;
+    
     it("Succesfully Grabs data information from the database, given a valid job id", async () => {
-        jobId = 1;
+        job_Id = jobId;
         await request(app)
         .get(`/jobs/edit/${jobId}`)
         .expect(200);
@@ -27,15 +42,32 @@ describe('GET /jobs', function(){
         await request(app)
         .get(`/jobs/edit/${jobId}`)
         .expect(400);
-    });  
+    });
+
+    Job.find({job_id: jobId}).remove().exec();
 });
 
 describe ('POST /jobs', function(){
+
+    let jobId;
+
+    const createJob = new Job({
+        job_id: 54321,
+        title: "Title Test",
+        description: "This is a description test.",
+        salary: 95300,
+        company: "InConnect",
+        category: "Full-Time",
+        location: "On-Site"
+    });
+    createJob.save();
+    jobId = createJob.job_id;
+
     it("Successfully updates job information, given valid data", async() => {
         await request(app)
-        .post('/jobs/edit/4')
+        .post(`/jobs/edit/${jobId}`)
         .send({
-            job_id: 4,
+            job_id: jobId,
             title: "Changed Title",
             description: "This is a changed description 333",
             salary: 12345,
@@ -59,9 +91,9 @@ describe ('POST /jobs', function(){
     })
     it("Fails to update job information, given valid id and invalid information", async() => {
         await request(app)
-        .post('/jobs/edit/5')
+        .post(`/jobs/edit/${jobId}`)
         .send({
-            job_id: 5,
+            job_id: jobId,
             title: 12345,
             description: "This is a changed description",
             salary: "Test",
@@ -70,4 +102,6 @@ describe ('POST /jobs', function(){
         })
         .expect(500)
     })
-})
+
+    Job.find({job_id: jobId}).remove().exec();
+});

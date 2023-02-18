cd422e5557df170cf73e92339357425e37784839
DasDeveloper <rohandas@live.ca>
2023-02-18 18:45:12 -0500

    Fixed two jobsController conflict

--- src/server/controllers/Jobscontroller.js
+++ src/server/controllers/Jobscontroller.js
@@ -1,5 +1,30 @@
-const Jobs = require("../models/Job.js");
+const Job = require("../models/Job");
+const bcrypt = require("bcrypt");
 
+
+
+
+const getAllJobs = async (req, res) => {
+
+    try{
+        const jobs = await Job.find();
+        res.status(200).json(jobs);
+      }catch(err){
+          res.status(500).json( {message: "Unable to retrieve all jobs."});
+          console.log(err);
+      }
+    };
+
+
+const getJobDetails = async(req, res) => {
+    try{
+        const job = await Job.findOne({job_id: req.params.jobId});
+        res.send(job);
+    }catch(err){
+        res.status[400].json({message: "Something went wrong."})
+    }
+}
+    
 const createJob = async (req, res) => {
   try {
     const { job_id, description, salary, company, category, title, location } =
@@ -21,14 +46,27 @@ const createJob = async (req, res) => {
   }
 };
 
-const getJobs = async (req, res) => {
-  try {
-    const jobs = await Jobs.find();
-    res.status(200).json(jobs);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Server Error" });
-  }
-};
+// const getJobs = async (req, res) => {
+//   try {
+//     const jobs = await Jobs.find();
+//     res.status(200).json(jobs);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ message: "Server Error" });
+//   }
+// };
 
-module.exports = { createJob, getJobs };
+
+
+
+
+
+
+module.exports = {
+    
+getAllJobs,
+getJobDetails,
+// getJobs,
+createJob
+
+}

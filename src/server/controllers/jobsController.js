de5a2b5199edc633919bc2d834f336252be96b13
DasDeveloper <rohandas@live.ca>
2023-02-18 19:05:48 -0500

    Update jobsController.js

--- src/server/controllers/jobsController.js
+++ src/server/controllers/jobsController.js
@@ -1,40 +1,34 @@
-const Job = require("../models/Job");
-const bcrypt = require("bcrypt");
+const Jobs = require("../models/Job.js");
 
+const createJob = async (req, res) => {
+  try {
+    const { job_id, description, salary, company, category, title, location } =
+      req.body;
+    const newJob = new Jobs({
+      job_id,
+      description,
+      salary,
+      company,
+      category,
+      title,
+      location,
+    });
+    const savedJob = await newJob.save();
+    res.status(200).json(savedJob);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
+const getJobs = async (req, res) => {
+  try {
+    const jobs = await Jobs.find();
+    res.status(200).json(jobs);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
-
-const getAllJobs = async (req, res) => {
-
-    try{
-        const jobs = await Job.find();
-        res.status(200).json(jobs);
-      }catch(err){
-          res.status(500).json( {message: "Unable to retrieve all jobs."});
-          console.log(err);
-      }
-    };
-
-
-const getJobDetails = async(req, res) => {
-    try{
-        const job = await Job.findOne({job_id: req.params.jobId});
-        res.send(job);
-    }catch(err){
-        res.status[400].json({message: "Something went wrong."})
-    }
-}
-    
-
-
-
-
-
-
-
-module.exports = {
-    
-getAllJobs,
-getJobDetails
-
-}
+module.exports = { createJob, getJobs };

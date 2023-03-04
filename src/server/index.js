9a1a4deba7179801c6643a748f27b36234bc4b7c
DasDeveloper <rohandas@live.ca>
2023-03-03 21:52:12 -0500

    Changed index.js to add routes for messages
    Co-Authored-By: AnnaHsu1 <78278972+AnnaHsu1@users.noreply.github.com>
    Co-Authored-By: Joe El-Khoury <78051064+Aboudik1452@users.noreply.github.com>
    Co-Authored-By: Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>

--- src/server/index.js
+++ src/server/index.js
@@ -16,31 +16,31 @@ const session_length = 1000 * 60 * 60;
 
 //MongoDB seission store
 const mongoDBstore = new MongoDBStore({
-  uri: process.env.DATABASE,
-  collection: "userSessions",
+    uri: process.env.DATABASE,
+    collection: "userSessions",
 });
 
 app.use(
-  session({
-    secret: "secret123",
-    name: "user_session_id",
-    store: mongoDBstore,
-    cookie: {
-      maxAge: session_length,
-      sameSite: false,
-      secure: false,
-    },
-    resave: true,
-    saveUninitialized: false,
-  })
+    session({
+        secret: "secret123",
+        name: "user_session_id",
+        store: mongoDBstore,
+        cookie: {
+            maxAge: session_length,
+            sameSite: false,
+            secure: false,
+        },
+        resave: true,
+        saveUninitialized: false,
+    })
 );
 
 //Cors middleware to accept request from client
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
 );
 
 //Supress DeprecationWarning message
@@ -48,18 +48,18 @@ mongoose.set("strictQuery", true);
 
 //Database connection (MongoDB Atlas)
 const dbConnect = () => {
-  mongoose
-    .connect(process.env.DATABASE)
-    .then(() => {
-      console.log("Database Connected Successfully");
-    })
-    .catch((err) => {
-      throw err;
-    });
+    mongoose
+        .connect(process.env.DATABASE)
+        .then(() => {
+            console.log("Database Connected Successfully");
+        })
+        .catch((err) => {
+            throw err;
+        });
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 //Routes for our API endpoints
@@ -68,11 +68,12 @@ app.use("/resume", require("./routes/uploadResumeCL.js"));
 app.use("/search", require("./routes/searchRoute.js"));
 app.use("/jobs", require("./routes/jobsRoutes.js"));
 app.use("/session", require("./routes/sessionRoutes.js"));
+app.use("/messages", require("./routes/messageRoutes.js"))
 
 //Running the server
 const server = app.listen(port, () => {
-  dbConnect();
-  console.log(`Server listening on port ${port}`);
+    dbConnect();
+    console.log(`Server listening on port ${port}`);
 });
 
 module.exports = server;

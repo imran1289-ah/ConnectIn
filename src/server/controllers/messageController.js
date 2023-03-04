537d6bde424864ab733ca540b13f2665c8c0e2a7
DasDeveloper <rohandas@live.ca>
2023-03-03 21:51:30 -0500

    Added messageController
    Co-Authored-By: AnnaHsu1 <78278972+AnnaHsu1@users.noreply.github.com>
    Co-Authored-By: Joe El-Khoury <78051064+Aboudik1452@users.noreply.github.com>
    Co-Authored-By: Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>

--- src/server/controllers/messageController.js
+++ src/server/controllers/messageController.js
@@ -1,1 +1,45 @@
-const Messages = require("../models/messageModel");
+const Messages = require("../models/message");
+
+
+const getMessages = async(req, res, next) => {
+    try {
+        const { from, to } = req.body;
+        const messages = await Messages.find({
+            users: {
+                $all: [from, to],
+            },
+        }).sort({ updatedAt: 1 });
+
+        const projectedMessages = messages.map((msg) => {
+            return {
+                fromSelf: msg.sender.toString() === from,
+                message: msg.text,
+            };
+        });
+        res.json(projectedMessages);
+    } catch (ex) {
+        next(ex);
+    }
+};
+
+const addMessages = async(req, res) => {
+    try {
+        const { from, to, message } = req.body;
+        const response = await Messages.create({
+            text: message,
+            users: [from, to],
+            sender: from
+        });
+
+        if (response)
+            res.json({ msg: "Message added successfully" })
+    } catch (err) {
+        res.status(400).json({ msg: "Unexpected error while adding message" })
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getMessages,
+    addMessages
+}

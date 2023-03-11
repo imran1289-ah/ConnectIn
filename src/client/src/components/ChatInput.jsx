065f1ccd8fb965d392e6637d2d39ba21034a323c
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2023-03-11 17:22:14 -0500

    file sharing

--- src/client/src/components/ChatInput.jsx
+++ src/client/src/components/ChatInput.jsx
@@ -1,31 +1,42 @@
-import React, {useState} from "react";
-import ChatInputCSS from "../css/ChatInput.module.css"
+import React, { useState } from "react";
+import ChatInputCSS from "../css/ChatInput.module.css";
 
 const ChatInput = ({ handleSendMsg }) => {
   const [message, setMessage] = useState("");
+  const [file, setFile] = useState(null);
 
   const sendChat = (e) => {
     e.preventDefault();
-    if (message.length > 0) {
-      handleSendMsg(message);
+    if (message.length > 0 || file) {
+      handleSendMsg({ message, file });
       setMessage("");
+      setFile(null);
     }
-  }
+  };
 
-return(
-  <>
-    <form onSubmit={(e) => sendChat(e)}>
-      <input
-        className={ChatInputCSS.chatbox}
-        type="text"
-        placeholder="Message"
-        onChange={(e) => setMessage(e.target.value)}
-        value={message}
-      />
-      <input className={ChatInputCSS.button} value="SEND" type="submit"></input>
-    </form>
-  </>
-);
-}
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  return (
+    <>
+      <form onSubmit={(e) => sendChat(e)}>
+        <input
+          className={ChatInputCSS.chatbox}
+          type="text"
+          placeholder="Message"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+        />
+        <input
+          className={ChatInputCSS.file}
+          type="file"
+          onChange={handleFileChange}
+        />
+        <input className={ChatInputCSS.button} value="SEND" type="submit" />
+      </form>
+    </>
+  );
+};
 
 export default ChatInput;

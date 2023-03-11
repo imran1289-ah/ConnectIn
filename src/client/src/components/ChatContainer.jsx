7e7bee014ca34fce0d6db48926615a2fa1909aa7
DasDeveloper <rohandas@live.ca>
2023-03-11 17:29:58 -0500

    Revert "file sharing"
    This reverts commit 13e03d5d30ae621e52614044087c381bc81d18ba.

--- src/client/src/components/ChatContainer.jsx
+++ src/client/src/components/ChatContainer.jsx
@@ -3,32 +3,45 @@ import { useState, useEffect, useRef } from "react";
 import ChatInput from "./ChatInput";
 import axios from "axios";
 
+
 const ChatContainer = ({ currentChat, socket }) => {
   const [messages, setMessages] = useState([]);
   const [arrivalMessage, setArrivalMessage] = useState(null);
 
-  // Get id of logged in user
+  //Get id of logged in user
   const userID = sessionStorage.getItem("userID");
 
   useEffect(() => {
-    fetchMessages();
-  }, [currentChat]);
+    fetchMessages()
+    },[currentChat]);
+  
+  
 
   const fetchMessages = async () => {
     try {
       if (userID) {
-        const response = await axios.post(`http://localhost:9000/messages`, {
-          from: userID,
-          to: currentChat.userID,
-        });
-        setMessages(response.data);
+        const response = await axios.post(
+          `http://localhost:9000/messages`,
+          {
+            from: userID,
+            to: currentChat.userID
+          }
+        );
+        console.log(response.data)
+        setMessages(response.data)
       }
+
     } catch (error) {
       console.log(error);
-    }
-  };
+    }    
+  }
 
   const handleSendMsg = async (msg) => {
+    // socket.current.emit("send-msg", {
+    //   to: currentChat._id,
+    //   from: userID,
+    //   msg,
+    // });
     await axios.post("http://localhost:9000/messages/addMessage", {
       from: userID,
       to: currentChat.userID,
@@ -37,63 +50,37 @@ const ChatContainer = ({ currentChat, socket }) => {
 
     const msgs = [...messages];
     msgs.push({ fromSelf: true, message: msg });
+    console.log(msgs);
     setMessages(msgs);
-  };
-
-  const downloadFile = async (fileUrl) => {
-    try {
-      const response = await axios({
-        url: fileUrl,
-        method: 'GET',
-        responseType: 'blob', // important
-      });
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', 'file');
-      document.body.appendChild(link);
-      link.click();
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   return (
     <div className={ContainerCSS.container}>
       <div className={ContainerCSS.header}>
-        <span>
-          {currentChat.firstname} {currentChat.lastname}
-        </span>
+        <span>{currentChat.firstname} {currentChat.lastname}</span>
       </div>
       <hr className={ContainerCSS.line} />
       <div className={ContainerCSS.chatBox}>
-        {messages.map((message, index) => (
-          <div key={index} className={ContainerCSS.messagesContainer}>
-            {message.fromSelf ? (
-              <div className={ContainerCSS.sent}>
-                <div className={ContainerCSS.textSent}>{message.message}</div>
-              </div>
-            ) : (
-              <div className={ContainerCSS.received}>
-                <div className={ContainerCSS.textReceived}>{message.message}</div>
-                {message.fileUrl && (
-                  <div className={ContainerCSS.downloadLink}>
-                    <a href="#" onClick={() => downloadFile(message.fileUrl)}>
-                      Download File
-                    </a>
-                  </div>
-                )}
-              </div>
-            )}
+        
+      {messages.map((message) => {
+        return(<div className={ContainerCSS.messagesContainer}>
+          {message.fromSelf ? (<div className={ContainerCSS.sent}>
+            <div className={ContainerCSS.textSent}>{message.message}</div>
+          </div>):
+          (<div className={ContainerCSS.received}>
+            <div className={ContainerCSS.textReceived}>{message.message}</div>
+            </div>)}
           </div>
-        ))}
+        )
+      })}
+
       </div>
       <hr className={ContainerCSS.line} />
       <div className={ContainerCSS.input}>
         <ChatInput handleSendMsg={handleSendMsg} />
       </div>
     </div>
-  );
-};
-
+    
+  ); 
+}
 export default ChatContainer;

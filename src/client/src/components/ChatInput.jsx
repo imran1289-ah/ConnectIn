536b9f68e91a70c19627ceeedb24a048cdf1bc7c
DasDeveloper <rohandas@live.ca>
2023-03-11 17:29:36 -0500

    Revert "file sharing"
    This reverts commit 065f1ccd8fb965d392e6637d2d39ba21034a323c.

--- src/client/src/components/ChatInput.jsx
+++ src/client/src/components/ChatInput.jsx
@@ -1,42 +1,31 @@
-import React, { useState } from "react";
-import ChatInputCSS from "../css/ChatInput.module.css";
+import React, {useState} from "react";
+import ChatInputCSS from "../css/ChatInput.module.css"
 
 const ChatInput = ({ handleSendMsg }) => {
   const [message, setMessage] = useState("");
-  const [file, setFile] = useState(null);
 
   const sendChat = (e) => {
     e.preventDefault();
-    if (message.length > 0 || file) {
-      handleSendMsg({ message, file });
+    if (message.length > 0) {
+      handleSendMsg(message);
       setMessage("");
-      setFile(null);
     }
-  };
+  }
 
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  return (
-    <>
-      <form onSubmit={(e) => sendChat(e)}>
-        <input
-          className={ChatInputCSS.chatbox}
-          type="text"
-          placeholder="Message"
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-        <input
-          className={ChatInputCSS.file}
-          type="file"
-          onChange={handleFileChange}
-        />
-        <input className={ChatInputCSS.button} value="SEND" type="submit" />
-      </form>
-    </>
-  );
-};
+return(
+  <>
+    <form onSubmit={(e) => sendChat(e)}>
+      <input
+        className={ChatInputCSS.chatbox}
+        type="text"
+        placeholder="Message"
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+      />
+      <input className={ChatInputCSS.button} value="SEND" type="submit"></input>
+    </form>
+  </>
+);
+}
 
 export default ChatInput;

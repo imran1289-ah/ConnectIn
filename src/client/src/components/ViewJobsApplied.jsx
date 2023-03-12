b2d7f647dea936e5b8b322e7c33a05051f54121c
Niniishere <annieliu190@gmail.com>
2023-03-11 22:05:33 -0500

    added logo

--- src/client/src/components/ViewJobsApplied.jsx
+++ src/client/src/components/ViewJobsApplied.jsx
@@ -1,76 +1,71 @@
 import React from "react";
-import axios from "axios"
+import axios from "axios";
 import "../css/viewJobsApplied.css";
-import {useEffect, useState} from "react";
-import {Link} from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import Avatar from '@mui/material/Avatar'
-import Button from '@mui/material/Button'
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
 import { Alert, AlertTitle } from "@mui/material";
-import WorkIcon from '@mui/icons-material/Work';
-import PlaceIcon from '@mui/icons-material/Place';
-import BusinessIcon from '@mui/icons-material/Business';
+import WorkIcon from "@mui/icons-material/Work";
+import PlaceIcon from "@mui/icons-material/Place";
+import BusinessIcon from "@mui/icons-material/Business";
 import { Context } from "../UserSession";
 import { useContext } from "react";
 import ArrowBack from "@mui/icons-material/ArrowBack";
 
+const ViewJobsApplied = () => {
+  const userID = sessionStorage.getItem("userID");
 
+  const [jobsApplied, setJobsApplied] = useState([]);
 
-const ViewJobsApplied = () =>{
+  const navigate = useNavigate();
 
-        
-        const userID = sessionStorage.getItem("userID");
-    
-        const [jobsApplied, setJobsApplied] = useState([]);
-    
-        const navigate = useNavigate();
+  useEffect(() => {
+    fetchAppliedJob();
+  }, []);
 
-        useEffect( () => {
-            fetchAppliedJob();
+  const fetchJobDetails = async () => {
+    jobsApplied.map();
+  };
 
-        }, [])
-    
-    
-    const fetchJobDetails = async () =>{
-        jobsApplied.map()
-    }
+  const fetchAppliedJob = async () => {
+    const { data } = await axios.get(`http://localhost:9000/users/${userID}/jobsApplied`);
 
-    const fetchAppliedJob = async () =>{
-        const {data} = await axios.get(`http://localhost:9000/users/${userID}/jobsApplied`)
-        
-       setJobsApplied(data)
-        
-    }
+    setJobsApplied(data);
+  };
 
-return(
+  return (
     <div className="jobsApplied">
-     
-            
-            <h1>Jobs Applied Summary</h1>
+      <h1>Jobs Applied Summary</h1>
 
-            
-            <table className="">
-              <tr>
-                <th>Job ID</th>
-                <th>Product</th>
-                <th>Quantity</th>
-                <th>Image</th>
-              </tr>
-              
-                {jobsApplied.map(id =>(
+      <table className="">
+        <tr>
+          <th>Job ID</th>
+          <th>Product</th>
+          <th>Quantity</th>
+          <th>Image</th>
+        </tr>
+        {jobsApplied.map(id => (
+          <div key={id} className="jobPost">
+            <div className="logo">
+              <Avatar alt="Logo" src="./logo/logo.png" sx={{ width: 75, height: 75 }} />
+            </div>
+            <tr>
+              <th>{id}</th>
+            </tr>
+          </div>
+        ))}
+        {/* {jobsApplied.map(id =>(
                     
                     <tr>
                         <th>{id}</th>
                     </tr>
                 ))}
-              
-
-              
-            </table> 
-
-
+               */}
+      </table>
     </div>
   );
-}
+};
 
 export default ViewJobsApplied;

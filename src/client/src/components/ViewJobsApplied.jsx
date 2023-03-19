1f20e5ad15f309f4f1d9bb446947c44ad9919a9a
DasDeveloper <rohandas@live.ca>
2023-03-18 21:13:04 -0400

    Adjusted view jobs applied
    Co-Authored-By: Joe El-Khoury <78051064+Aboudik1452@users.noreply.github.com>

--- src/client/src/components/ViewJobsApplied.jsx
+++ src/client/src/components/ViewJobsApplied.jsx
@@ -51,7 +51,7 @@ const ViewJobsApplied = () => {
   };
 
   const fetchAppliedJob = async () => {
-    const { data } = await axios.get(`http://localhost:9000/users/${userID}/jobsObjectApplied`);
+    const { data } = await axios.get(`http://localhost:9000/users/${userID}/jobsApplied`);
 
     setJobsApplied(data);
   };

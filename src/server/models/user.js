f9272a9e40610c20c01fc6efed1743e683172476
DasDeveloper <rohandas@live.ca>
2023-03-18 21:10:30 -0400

    Removed the jobsAppliedObject attribute

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -32,7 +32,6 @@ const UserSchema = new mongoose.Schema({
         type: Number,
     },
     jobsApplied: [],
-    jobsAppliedObject:[],
     volunteering: [],
     connections: [],
     education: [],

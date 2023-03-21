6422185e6a55ba95f3c8d5c18af338b08dc8b6f4
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-03-21 08:28:01 -0400

    adding a select button to change langauge

--- src/client/src/components/LoginFooter.jsx
+++ src/client/src/components/LoginFooter.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import "../css/loginfooter.css";
 
 const Footer = () => {
+  const [language, setLanguage] = React.useState("");
+
+  const handleChange = (event) => {
+    setLanguage(event.target.value);
+  };
+
   return (
     // Footer of the page
     <div className="FooterLine">
       <div className="Info">
-        Home | About | Languages
+        Home | About |
+        <select>
+          <option value="en" onChange={handleChange}>
+            English
+          </option>
+          <option value="fr" onChange={handleChange}>
+            French
+          </option>
+        </select>
         <br></br>
         ConnectIn @2023
       </div>

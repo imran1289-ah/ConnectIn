cfa7c89e234b608f983fc0e5de99233ceab1e4d9
Meet Vora <meetvora02@gmail.com>
2023-03-21 17:28:10 -0400

    Recruiter id and worktype works!

--- src/client/src/components/JobPosting.jsx
+++ src/client/src/components/JobPosting.jsx
@@ -6,33 +6,33 @@ import "../css/JobPosting.css";
 import axios from "axios";
 
 const JobPosting = () => {
-    //Global loginState
-    const [login, setLogin] = useContext(Context);
+  //Global loginState
+  const [login, setLogin] = useContext(Context);
 
-    //Get id of logged in user
-    const userID = sessionStorage.getItem("userID");
-    const userRole = sessionStorage.getItem("role");
-  
-    useEffect(() => {
+  //Get id of logged in user
+  const userID = sessionStorage.getItem("userID");
+  const userRole = sessionStorage.getItem("role");
+
+  useEffect(() => {
+    if (userID) {
+      fetchSession();
+    }
+  }, []);
+
+  //Having the loginState persist on all pages
+  const fetchSession = async () => {
+    try {
       if (userID) {
-        fetchSession();
+        setLogin({
+          isLoggedIn: true,
+        });
       }
-    }, []);
-  
-    //Having the loginState persist on all pages
-    const fetchSession = async () => {
-      try {
-        if (userID) {
-          setLogin({
-            isLoggedIn: true,
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  if (userID && (userRole === "Recruiter" || userRole === 'Administrator')) {
+  if (userID && (userRole === "Recruiter" || userRole === "Administrator")) {
     return (
       <div className="Jobpostingform">
         <div className="title">
@@ -83,6 +83,17 @@ const JobPosting = () => {
         </div>
         <br />
         <br />
+        <div className="work_type">
+          <TextField
+            id="work_type"
+            label="worktype"
+            placeholder="Placeholder"
+            variant="outlined"
+          />
+          <FaRegEdit />
+        </div>
+        <br />
+        <br />
         <button className="button" onClick={() => createJob()}>
           Post/Save
         </button>
@@ -91,12 +102,12 @@ const JobPosting = () => {
   } else {
     return (
       <div className="Jobpostingform">
-        <h1 style={{ textAlign: "center" }}>You need to be a recruiter
-        or an administrator to post jobs!</h1>
+        <h1 style={{ textAlign: "center" }}>
+          You need to be a recruiter or an administrator to post jobs!
+        </h1>
       </div>
     );
   }
-    
 };
 
 export default JobPosting;
@@ -105,12 +116,14 @@ const createJob = async () => {
   try {
     const jobData = {
       job_id: Math.floor(Math.random() * 1000000), // generate a random job_id for testing
+      recruiter_id: sessionStorage.getItem("userID"),
       title: document.getElementById("job_title").value,
       company: document.getElementById("company_name").value,
       description: document.getElementById("job_description").value,
       salary: document.getElementById("salary").value,
       category: document.getElementById("category").value,
       location: document.getElementById("location").value,
+      work_type: document.getElementById("work_type").value,
     };
     const response = await axios
       .post(`http://localhost:9000/jobs/create`, jobData)

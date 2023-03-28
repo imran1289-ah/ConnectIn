7956fbb459e3c139789e613274d8806443560506
AnnaHsu1 <annahsu2000@hotmail.com>
2023-03-27 22:22:17 -0400

    Added css for dmReports page

--- src/client/src/components/DMReport.jsx
+++ src/client/src/components/DMReport.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "../css/dmReports.css";
 
 const DMReport = () => {
 
   const [reports, setReports] = useState([]);
 
-  useEffect =(() => {
+  useEffect(() => {
     fetchReports();
-  }, [])
+  }, []);
 
   const fetchReports = async () => {
-    const data = await axios.get("http://localhost:9000/reports")
+    const {data} = await axios.get("http://localhost:9000/reports")
+    console.log(data)
     setReports(data)
   }
 
@@ -37,15 +39,19 @@ const DMReport = () => {
           <b>DM Reports</b>
         </div>
         <div className="dm-reports">
-        {reports.map(report => {
+        {reports.map(report => (
           <div key={report._id} className="report">
-            <p>{report.sender}</p>
-            <p>{report.receiver}</p>
-            <p>{report.reportedMessage}</p>
-            <p>{report.justification}</p>
-            <button onClick={deleteReport(`${report._id}`)}></button>
+            <p><span style={{fontWeight: "bold"}}>Sender: </span>{report.sender}</p>
+            <p><span style={{fontWeight: "bold"}}>Receiver: </span>{report.receiver}</p>
+            <p><span style={{fontWeight: "bold"}}>Message: </span>{report.reportedDM}</p>
+            <p><span style={{fontWeight: "bold"}}>Justification: </span>{report.justification}</p>
+            <div className="report-button-bundle">
+              <button className="report-button-accept" type="button">Accept</button>
+              <button className="report-button-reject" type="button">Reject</button>
+            </div>
+            {/* <button type="button" onClick={deleteReport(`${report._id}`)}></button> */}
           </div>
-        })}
+        ))}
         </div>
       </div>
     </div>

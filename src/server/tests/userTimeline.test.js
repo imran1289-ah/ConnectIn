924702277bf80d3ed66ecd8a0d4edbb6c18e762c
AnnaHsu1 <78278972+AnnaHsu1@users.noreply.github.com>
2023-04-01 23:15:37 -0400

    Add backend tests for the users controller (#178)

--- src/server/tests/userTimeline.test.js
+++ src/server/tests/userTimeline.test.js
@@ -2,10 +2,16 @@ const request = require("supertest");
 const app = require("../index.js");
 const mongoose = require("mongoose");
 const User = require("../models/user");
+
+afterAll(async() => {
+  await mongoose.disconnect();
+  app.close();
+})
+
 describe("Posts", () => {
   let userID;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     mongoose.connect(process.env.DATABASE);
     const res = await request(app).post("/users").send({
       firstname: "Hello",
@@ -15,20 +21,14 @@ describe("Posts", () => {
       role: "User"
     });
     userID = res.body.id;
+    console.log(userID)
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     // Remove the test from the database
     await User.findByIdAndDelete(userID);
   });
-
-  afterAll(done => {
-    mongoose.disconnect();
-    done();
-    app.close();
-  });
-
-  it("succesful new post creation", async () => {
+    it("succesful new post creation", async () => {
     const res = await request(app).post("/users/post").send({
       _id: userID,
       firstname: "Hello",
@@ -37,5 +37,15 @@ describe("Posts", () => {
       timestamp: Date.now()
     });
     expect(200);
-  });
+    });
+
+  
+  describe("GET /:id/posts", () => {
+    it("Successfully fetches the posts made by user", async() => {
+      const res = await request(app)
+      .get(`/users/${userID}/posts`);
+
+      expect(res.statusCode).toEqual(200);
+    })
+  })
 });

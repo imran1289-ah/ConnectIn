676db3203f91c0122d4e47112988851cae35807d
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2023-04-17 12:00:01 -0400

    small change in search page

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -80,7 +80,7 @@ function App() {
             path="/users/search"
             element={
               <>
-                <Navbar /> <SearchUser /> <LoginFooter />
+                <Navbar /> <SearchUser /> <SignupFooter />
               </>
             }
           ></Route>
